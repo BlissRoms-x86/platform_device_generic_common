a04e304cdf494d14f01088f84035a59fd60c8722
Unknown <electrikjesus@gmail.com>
2017-12-10 13:51:01 -0500

    Disable the few things not working just yet

--- treble.mk
+++ treble.mk
@@ -2,7 +2,6 @@
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service
 
 # HWComposer HAL
 PRODUCT_PACKAGES += \
@@ -39,16 +38,16 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service
 
 # Gatekeeper HAL
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl
+# PRODUCT_PACKAGES += \
+#    android.hardware.gatekeeper@1.0-impl
 
+# GNSS HAL 
+# PRODUCT_PACKAGES += \ 
+#     android.hardware.gnss@1.0-impl 
+    
 # Health HAL 
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl
-
-# GNSS HAL
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
@@ -59,13 +58,13 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
 
 # Memtrack HAL
-PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl
+# PRODUCT_PACKAGES += \
+#     android.hardware.memtrack@1.0-impl
 
 # NFC HAL
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service
+# PRODUCT_PACKAGES += \
+#     android.hardware.nfc@1.0-impl \
+#     android.hardware.nfc@1.0-service
     
 # Default OMX service to non-Treble
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -85,8 +84,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
 # Thermal HAL
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
+# PRODUCT_PACKAGES += \
+#     android.hardware.thermal@1.0-impl
 
 # USB HAL
 PRODUCT_PACKAGES += \
@@ -94,8 +93,8 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+# PRODUCT_PACKAGES += \
+#     android.hardware.vibrator@1.0-impl
 
 # Wifi HAL
 PRODUCT_PACKAGES += \
