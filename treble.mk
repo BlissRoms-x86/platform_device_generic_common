29e05fa18d53a13c69c9dbea018e882e1a2982c6
Unknown <electrikjesus@gmail.com>
2017-12-11 21:10:34 -0500

    add graphics allocator service

--- treble.mk
+++ treble.mk
@@ -2,7 +2,8 @@
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.allocator@2.0-impl \
-
+    android.hardware.graphics.allocator@2.0-service 
+    
 # HWComposer HAL
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl
